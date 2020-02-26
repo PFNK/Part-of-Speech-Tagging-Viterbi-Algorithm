3f8da82d7e084adb73a3389f05361923683d2c7d
Maria Sliacka <s1763241@froome.inf.ed.ac.uk>
2020-02-26 16:30:54 +0000

    small fixes

--- template.py
+++ template.py
@@ -65,11 +65,12 @@ class HMM:
         :return: The emission probability distribution and a list of the states
         :rtype: Tuple[ConditionalProbDist, list(str)]
         """
-        # raise NotImplementedError('HMM.emission_model')
+
         # TODO prepare data
 
         def lidstone_est(f):
             return nltk.probability.LidstoneProbDist(f,0.01,f.B()+1)
+
         # Don't forget to lowercase the observation otherwise it mismatches the test data
         # Do NOT add <s> or </s> to the input sentences
         data = [(tag, word.lower()) for (word, tag) in list(itertools.chain.from_iterable(train_data))]
@@ -96,7 +97,7 @@ class HMM:
         :return: log base 2 of the estimated emission probability
         :rtype: float
         """
-        # raise NotImplementedError('HMM.elprob')
+
         return math.log(self.emission_PD[state].prob(word), 2)
 
     # Compute transition model using ConditionalProbDist with a LidstonelprobDist estimator.
@@ -110,7 +111,7 @@ class HMM:
         :return: The transition probability distribution
         :rtype: ConditionalProbDist
         """
-        # raise NotImplementedError('HMM.transition_model')
+
         # TODO: prepare the data
         # The data object should be an array of tuples of conditions and observations,
         # in our case the tuples will be of the form (tag_(i),tag_(i+1)).
@@ -146,7 +147,7 @@ class HMM:
         :return: log base 2 of the estimated transition probability
         :rtype: float
         """
-        # raise NotImplementedError('HMM.tlprob')
+
         return math.log(self.transition_PD[state1].prob(state2), 2)
 
     # Train the HMM
@@ -170,7 +171,7 @@ class HMM:
         :param observation: the first word in the sentence to tag
         :type observation: str
         """
-        # raise NotImplementedError('HMM.initialise')
+
         # Initialise step 0 of viterbi, including
         #  transition from <s> to observation
         # use costs (-log-base-2 probabilities)
@@ -200,58 +201,36 @@ class HMM:
         :return: List of tags corresponding to each word of the input
         :rtype: list(str)
         """
-        # raise NotImplementedError('HMM.tag')
+
         tags = []
 
-        for t in range(1, len(observations)):  # fixme to iterate over steps step = word
-            for s in self.states:  # fixme to iterate over states state = tag
-                # fixme to update the viterbi and backpointer data structures
-                #  Use costs, not probabilities
-                # here for each row (tag/state) get value on previous word and
-                # multiply with current values
+        for t in range(1, len(observations)):  #iterate over steps where step = word in the sentence
+            for s in self.states:  #iterate over states where state = tag
+                # update the viterbi and backpointer data structures
+                # use costs, not probabilities
+
                 tmp_values = [(self.get_viterbi_value(state, t - 1)
-                              - math.log(self.transition_PD[state].prob(s), 2) # prob of going from that row to curren state
+                              - math.log(self.transition_PD[state].prob(s), 2)
                               - math.log(self.emission_PD[s].prob(observations[t]), 2), state)
                               for state in self.states]
                 min_val = min(tmp_values, key=lambda item: item[0])
                 self.viterbi[s].append(min_val[0])
                 self.backpointer[s].append(min_val[1])
 
-        # TODO
-        # Add a termination step with cost based solely on cost of transition to </s> , end of sentence.
-        # for state in self.states:
-        #     # tmp_value = - self.get_viterbi_value(state, len(observations) - 1)
-        #     # - math.log(self.transition_PD['</s>'].prob(state), 2)
-        #     # - math.log(self.emission_PD[state].prob(observations[t]))
-        #
-        #     tmp_value = - math.log(self.transition_PD[state].prob('</s>'), 2)
-        #
-        #     self.viterbi[state].append(tmp_value)
-        #     # self.backpointer[state].append('</s>')
-        #
-        # # best_last_tag = min([(- math.log(self.transition_PD[state].prob('</s>'), 2), state)
-        #                     #    for state in self.states], key=lambda item: item[0])[1]
-        # best_last_tag = min([(self.viterbi[state][len(observations)], state)
-        #                         for state in self.states], key=lambda item: item[0])[1]
+        # termination step with cost based solely on cost of transition to </s> , end of sentence.
         best_last_tag = min([(- math.log(self.transition_PD[state].prob('</s>'), 2) + self.get_viterbi_value(state, len(observations) -1), state)
                                 for state in self.states], key=lambda item: item[0])[1]
-        # TODO
+
         # Reconstruct the tag sequence using the backpointer list.
         # Return the tag sequence corresponding to the best path as a list.
         # The order should match that of the words in the sentence.
         tags.append(best_last_tag)
-        print(observations)
-        print('\n')
-        # print(self.backpointer)
-        # print('\n')
-        # print(self.viterbi)
-        # print('\n')
 
         for step in range(len(observations)-1, 0, -1):
             tags.append(self.get_backpointer_value(tags[len(observations) - 1 - step],step))
-            #print(tags)
+
         tags.reverse()
-        print(tags)
+
         return tags
 
     # Access function for testing the viterbi data structure
@@ -269,7 +248,7 @@ class HMM:
         :return: The value (a cost) for state as of step
         :rtype: float
         """
-        # raise NotImplementedError('HMM.get_viterbi_value')
+
         return self.viterbi[state][step]
 
     # Access function for testing the backpointer data structure
@@ -287,7 +266,7 @@ class HMM:
         :return: The state name to go back to at step-1
         :rtype: str
         """
-        # raise NotImplementedError('HMM.get_backpointer_value')
+
         return self.backpointer[state][step]
 
 
@@ -428,7 +407,7 @@ def answers():
             else:
                 incorrect += 1  # fix me
 
-    accuracy = correct/(correct + incorrect)  # fix me
+    accuracy = 1. * correct/(correct + incorrect)  # fix me
     print('Tagging accuracy for test set of %s sentences: %.4f' % (test_size, accuracy))
 
     # Print answers for 4b, 5 and 6
